ce614fe2bf1af7169487421e24b8c21de1fe237f
schmalle <schmall.markus@gmail.com>
2017-08-28 09:34:27 +0200

    optimised mapping

--- requests/addmapping.sh
+++ requests/addmapping.sh
@@ -29,14 +29,7 @@ curl -XPUT 'localhost:9200/ews2017.1/_mapping/Alert?pretty' -H 'Content-Type: ap
 {
   "properties": {
 
-                    "recievedTime": {
-                        "type": "date",
-                        "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
-                    },
-                    "createTime": {
-                        "type": "date",
-                        "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
-                    },
+                    
                     "createTime": {
                         "type": "date",
                         "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
