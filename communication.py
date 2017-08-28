c2d3d93d531dfe2069d6b8f5462caeee891bff22
schmalle <schmall.markus@gmail.com>
2017-08-28 10:13:59 +0200

    fixes

--- communication.py
+++ communication.py
@@ -1,9 +1,12 @@
 from slackclient import SlackClient
 
 #
-#
+# simple Slack sender for status reports
 #
 def sendSlack(channel, token, message):
+
+    if channel is None or token is None or message is None:
+        return
 
     sc = SlackClient(token)
 
